760e7564cd70edac0203eb8bf6cca94ff45799eb
Gulnaz Omirzakova <103216968+oonoky@users.noreply.github.com>
2024-04-10 10:16:54 +0600

    task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
 
@@ -12,7 +12,8 @@ import { WeatherComponent } from './weather/weather.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,57 +1,82 @@
-import { WeatherService } from './../weather.service';
-import { Component, OnInit } from '@angular/core';
+// import { WeatherService } from './../weather.service';
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-weather',
+//   templateUrl: './weather.component.html',
+//   styleUrls: ['./weather.component.css']
+// })
+// export class WeatherComponent implements OnInit {
+//   myWeather: any;
+//   temperature: number = 0;
+//   feelsLikeTemp: number = 0;
+//   humidity: number = 0;
+//   pressure: number = 0;
+//   summary: string = '';
+//   iconURL: string = '';
+//   city: string = 'Almaty';
+//   //units: string = 'metric'
+//   units: string = 'imperial'
+
+//   constructor(private weatherService: WeatherService) { }
+
+//   ngOnInit(): void {
+//     this.getWeather();
+//   }
+
+//   getWeather() {
+//     this.weatherService.getweather(this.city, this.units).subscribe({
+//       next: (res) => {
+//         console.log(res);
+//         this.myWeather = res;
+//         this.temperature = this.myWeather.main.temp;
+//         this.feelsLikeTemp = this.myWeather.main.feels_like;
+//         this.humidity = this.myWeather.main.humidity;
+//         this.pressure = this.myWeather.main.pressure;
+//         this.summary = this.myWeather.weather[0].main;
+  
+//         // Corrected the URL by removing extra space
+//         this.iconURL = 'https://openweathermap.org/img/wn' + this.myWeather.weather[0].icon + '@2x.png';
+//       },
+//       error: (error) => console.log(error.message),
+//       complete: () => console.info('API call completed')
+//     });
+//   }
+  
+
+//   onRadioButtonChange() {
+//     if (this.units == 'imperial') {
+//       this.units = 'metric';
+//     } else {
+//       this.units = 'imperial';
+//     }
+
+//     this.getWeather();
+//   }
+
+// }
+
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css']
 })
-export class WeatherComponent implements OnInit {
-  myWeather: any;
-  temperature: number = 0;
-  feelsLikeTemp: number = 0;
-  humidity: number = 0;
-  pressure: number = 0;
-  summary: string = '';
-  iconURL: string = '';
-  city: string = 'Almaty';
-  //units: string = 'metric'
-  units: string = 'imperial'
+export class WeatherComponent {
+  cityName !: string;
+  weatherData: any;
 
-  constructor(private weatherService: WeatherService) { }
-
-  ngOnInit(): void {
-    this.getWeather();
-  }
+  constructor(private http: HttpClient) {}
 
   getWeather() {
-    this.weatherService.getweather(this.city, this.units).subscribe({
-      next: (res) => {
-        console.log(res);
-        this.myWeather = res;
-        this.temperature = this.myWeather.main.temp;
-        this.feelsLikeTemp = this.myWeather.main.feels_like;
-        this.humidity = this.myWeather.main.humidity;
-        this.pressure = this.myWeather.main.pressure;
-        this.summary = this.myWeather.weather[0].main;
-  
-        // Corrected the URL by removing extra space
-        this.iconURL = 'https://openweathermap.org/img/wn' + this.myWeather.weather[0].icon + '@2x.png';
-      },
-      error: (error) => console.log(error.message),
-      complete: () => console.info('API call completed')
+    const apiKey = '5cad434697f1def9900049dc08df685a';
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${this.cityName}&appid=${apiKey}&units=metric`;
+    
+    this.http.get(url).subscribe((data: any) => {
+      this.weatherData = data;
     });
   }
-  
-
-  onRadioButtonChange() {
-    if (this.units == 'imperial') {
-      this.units = 'metric';
-    } else {
-      this.units = 'imperial';
-    }
-
-    this.getWeather();
-  }
-
 }
+
